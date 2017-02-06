7a1a6d1e553d37d7d1a7180e4e4147d6bf18c3e3
Kim Ranyard <kranyard@vmware.com>
2017-02-06 15:18:35 +0000

    SCripts

--- scm-blueprint.sh
+++ scm-blueprint.sh
@@ -1,6 +1,44 @@
 #!/bin/bash
 
-BLUEPRINT=CentOSTest
+cd /root/vra-demo
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -h|--host)
+    VRA_HOST="$2"
+    shift # past argument
+    ;;
+    -t|--tenant)
+    TENANT="$2"
+    shift # past argument
+    ;;
+    -u|--username)
+    USERNAME="$2"
+    shift # past argument
+    ;;-p|--password)
+    PASSWORD="$2"
+    shift # past argument
+    ;;
+    -bp|--blueprint)
+    BLUEPRINT="$2"
+    shift # past argument
+    ;;
+    -a|--author)
+    AUTHOR="$2"
+    shift # past argument
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
 
 ./do_export.py $BLUEPRINT
 
